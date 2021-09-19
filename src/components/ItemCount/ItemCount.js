884876643f27e688647c6e2e036c3dd51f742a78
Sebastian Aranda <sebastian.aranda.12@gmail.com>
2021-09-19 18:45:43 -0300

    Agrego desafio de Contador con boton

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -1,14 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
 import { faMinus } from '@fortawesome/free-solid-svg-icons'
 
 const ItemCount = () => {
+
+    const [stock, setStock] = useState(5)
+    const [initial, setInitial] = useState(1);
+
+    useEffect(() => {
+        console.log('Stock restante: ' + stock);
+        if (stock > 0) {
+            setInitial(1);
+        } else {
+            setInitial(0);
+        }
+    }, [stock]);
+
+    const agregarItem = () => {
+        if (initial < stock) {
+            setInitial(initial + 1);
+        }
+    };
+    const eliminarItem = () => {
+        if (initial > 0) {
+            setInitial(initial - 1);
+        }
+    };
+    const agregarStock = () => {
+        if (initial > 0) {
+            setStock(stock - initial);
+        }
+    };
+
     return (
         <div className="itemCount">
-            <FontAwesomeIcon icon={faMinus} />
-            <p>0</p>
-            <FontAwesomeIcon icon={faPlus} />
+            <div className="itemCount__controls">
+                <FontAwesomeIcon icon={faMinus} onClick={eliminarItem} />
+                <p>{initial}</p>
+                <FontAwesomeIcon icon={faPlus} onClick={agregarItem} />
+            </div>
+            <button onClick={agregarStock}>Agregar al carrito</button>
         </div>
     )
 }
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -5,13 +5,13 @@ import Product from "../Product/Product";
 const ItemListContainer = () => {
     return (
         <div className="listContainer">
+            <ItemCount />
             <Product title="Reloj 1" price="$1400" />
             <Product title="Reloj 2" price="$1600" />
             <Product title="Reloj 3" price="$5040" />
             <Product title="Reloj 4" price="$1400" />
             <Product title="Reloj 5" price="$1600" />
             <Product title="Reloj 6" price="$5040" />
-            <ItemCount />
         </div>
     )
 }
